0c0853fd94a55691afeb4ce96af2815b7cd53cc5
parksoohee <psk7489@naver.com>
2025-01-20 11:14:27 +0900

    내려오는 초 수정

--- js/hamburger.js
+++ js/hamburger.js
@@ -33,7 +33,7 @@ $(function(){
   /*#gnb>li에 마우스를 올리면 그 li 안에 있는 .sub가 슬라이드되서 내려오고,
   #gnb>li에서 마우스가 나가면 내려와있던 .sub가 슬라이드되서 올라간다.*/
   $('.nav>li').mouseenter(function(){
-      $(this).children('.sub_menu').stop().slideDown(500);
+      $(this).children('.sub_menu').stop().slideDown(300);
       /*stop부분을 넣지 않으면 슬라이드다운되서 내려오는 중에 마우스가 나가면 sub혼자 움직이는 현상이 발생하므로 마우스가 도중에 나갈 때 행동이 취소 될 수 있게 한다.*/
   });
   $('.nav>li').mouseleave(function(){
